195cf2ae4602803ecfc98ed87a986bf03d4d1325
Eron Powell <erondpowell@gmail.com>
2022-12-27 01:15:26 -0600

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "DevTalk.fun",
+  titleTemplate: "The FUN place to talk dev stuff",
+  lang: "en-US",
+  themeConfig: {
+    siteTitle: "DevTalk.fun",
+    algolia: {
+      appId: "",
+      apiKey: "",
+      indexName: "devtalk",
+    },
+    nav: [{ text: "Guide", link: "/guide" }],
+    socialLinks: [{ icon: "github", link: "https://github.com/erondpowell" }],
+    sidebar: [
+      {
+        text: "TypeScript",
+        collapsible: true,
+        collapsed: true,
+        items: [
+          { text: "What's TypeScript?", link: "/ts/whats-typescript.md" },
+          { text: "Basic Types", link: "/ts/basic-types" },
+        ],
+      },
+    ],
+    editLink: {
+      pattern: "https://github.com/vuejs/devtalk/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+    footer: {
+      message: "Released under no License. Just don't copy my stuff.",
+      copyright: "Copyright © 2022-present Eron Donevan Powell",
+    },
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,23 @@
+---
+layout: home
+
+hero:
+  name: DevTalk.fun
+  text: A blog and forum for all things dev.
+  tagline: It's the FUN way to learn tech.
+  actions:
+    - theme: brand
+      text: What's Typescript?
+      link: /ts/whats-typescript.md
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/vuejs/vitepress
+
+features:
+  - title: A cool blog
+    details: Blog content that doesn't suck.
+  - title: A "highly dank" forum
+    details: Talk to your peers in style.. on Discord!
+  - title: Open Source Genes
+    details: Got something to share? PRs welcome.
+---
